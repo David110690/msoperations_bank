a9f62d01a51f854720d55a12540b306944382cc4
Dtravezano <Davidu.0622@gmail.com>
2021-11-22 16:31:34 -0500

    config pom

--- src/main/java/NTTDATA/msoperations_bank/MsoperationsBankApplication.java
+++ src/main/java/NTTDATA/msoperations_bank/MsoperationsBankApplication.java
@@ -2,7 +2,9 @@ package NTTDATA.msoperations_bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MsoperationsBankApplication {
 
